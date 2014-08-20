ddd63f39c9201ae0118e3d28b75acf336356d65d
eranyil <user@172-2-1-164.lightspeed.dybhfl.sbcglobal.net>
2014-08-20 14:06:41 +0300

    hac2

---
+++ index 2.php
@@ -0,0 +1,371 @@
+<!-- This website is designed by Erhan YILDIRIM 01.03.2011 -->
+<html>
+
+<head>
+<meta name="google-site-verification" content="E6LAHdLVT_IvrTYGagD2jpON6fK107E25fzjAsJgCH8" />
+<link rel="Shortcut Icon" href="http://www.icdenetimmerkezi.com/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon"> 
+<link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/dmi_css.css" />
+
+<link rel="stylesheet" type="text/css" media="screen" href="css/purecss.css" />
+
+<title>İ&ccedil; Denetim Merkezi</title>
+<link rel="SHORTCUT ICON" href="/favicon.ico">
+<?php include("phpheader.php"); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-9">
+<meta name="Title" CONTENT="İDM İç Denetim Merkezi">
+<meta name="Description" CONTENT="İç Denetim Hakkında Aradığınız Herşey">
+<meta name="Keywords" CONTENT="İç Denetim Merkezi – Risk, İç Kontrol, Kurumsal Yönetim, iç denetim,iç denetçi eğitimi,iç denetçi,iç denetim raporu,iç denetim koordinasyon,iç denetim eğitimleri,iç denetim soru listesi,iç denetim sertifikası,iç denetim rapor örneği,iç denetim soruları,iç denetçi sertifikası,iç denetim nedir,iç denetçi nedir,iç denetim standartları,iç denetim derneği,iç denetim enstitüsü,iç denetim müdürü,iç denetim yönetmeliği,iç denetim uzmanı,iç denetim planı,iç denetim prosedürü,iç denetim dergisi,risk odaklı iç denetim,iç kontrol,iç kontrol sistemi,iç kontrol standartları,iç kontrol nedir,coso iç kontrol,risk yönetimi,risk ve risk yönetimi,kriz yönetimi,risk analizi,risk,analizleri,yönetim eğitimleri,kamusal eğitim,risk değerlendirme,sigortacılıkta risk yönetimi,risk yönetimi nedir,kurumsal risk yönetimi,sigortacılık ve risk yönetimi,sigorta ve risk yönetimi,işletmelerde risk yönetimi,kurumsal yönetim,kurumsallaşma,kurumsal yönetim ilkeleri,kurumsallaşma nedir,şirketlerde kurumsallaşma,aile şirketlerinde kurumsallaşma,kurumsal yönetişim,kurumsal yönetim derecelendirme,kurumsal yönetim nedir,kurumsallaşma süreci,kurumsallaşma nasıl olur,bilgi güvenliği,bilgi güvenlik,bilgigüvenliği,bilgi sistemleri güvenliği,güvenlik hakkında bilgi,bilgi güvenliği teknolojisi,bilgi güvenliği yönetim sistemi,bilgi güvenliği nedir,bilgi güvenliği politikası">
+<meta name="Author" CONTENT="İç Denetim Merkezi">
+<meta name="GOOGLEBOT" CONTENT="all, INDEX, FOLLOW">  
+<meta name="robots" content="all, index, follow">
+<meta name="author" content="icdenetimmerkezi.com">
+<meta name="category" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="abstract" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="search" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="distribution" content="global">
+<meta name="classification" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="date-creation-yyyy" content="2000">
+<meta name="dmoz" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="altavista" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="scooter" content="iç denetim, iç kotrol, risk yönetimi">
+<meta name="rating" content="general">
+<meta name="revisit-after" content="1 day">
+<meta name="search" content="iç denetim, iç kotrol, risk yönetimi">
+
+
+<script type="text/javascript">
+
+<!--
+
+function MM_swapImgRestore() { //v3.0
+
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+
+}
+
+function MM_preloadImages() { //v3.0
+
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+
+}
+
+
+
+function MM_findObj(n, d) { //v4.01
+
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+
+}
+
+
+
+function MM_swapImage() { //v3.0
+
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+
+}
+
+//-->
+
+</script>
+
+
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+
+</head>
+
+<body background="images/site_bg.jpg"  leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" class="body" onLoad="MM_preloadImages('images/buttons/bilgi__but_mo.gif','images/buttons/basvuru_but_mo.gif','images/buttons/ankete_katilin_mo.gif')">
+
+<!-- Save for Web Slices (template086.psd) -->
+<table width="1000" border="0" align="center" cellpadding="1" cellspacing="0" id="Table_00" bgcolor="#999999">
+	<tr><td>
+        <table width="1000" border="0" align="center" cellpadding="3" cellspacing="0" id="Table_01" bgcolor="#FFFFFF">
+	<tr>
+		<td align="center" colspan="2">
+       		<?php include("header_ustbant.php"); ?>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">  
+			<!--<img src="images/top2.gif" width="775" height="38" alt="">--></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+       <table width="100%" border="0" bgcolor="#FFFFFF" class="tbl">
+        <tr>
+        <td valign="top" bgcolor="#EAEEEE">
+       <table border="0" class="tbl">
+  			<tr>
+   				 <td valign="top">
+    				<table width="100%" border="0">
+  						<tr>
+    					<td valign="top"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="740" height="350" id="FlashID" title="ic denetim merkezi">
+  						    <param name="movie" value="home.swf">
+  						    <param name="quality" value="high">
+  						    <param name="wmode" value="opaque">
+  						    <param name="swfversion" value="6.0.65.0">
+  						    <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+  						    <param name="expressinstall" value="Scripts/expressInstall.swf">
+  						    <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+  						    <!--[if !IE]>-->
+  						    <object type="application/x-shockwave-flash" data="home.swf" width="740" height="350">
+  						      <!--<![endif]-->
+  						      <param name="quality" value="high">
+  						      <param name="wmode" value="opaque">
+  						      <param name="swfversion" value="6.0.65.0">
+  						      <param name="expressinstall" value="Scripts/expressInstall.swf">
+  						      <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+  						      <div>
+  						        <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+  						        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+						        </div>
+  						      <!--[if !IE]>-->
+						      </object>
+  						    <!--<![endif]-->
+
+						    </object></td>
+
+    					
+
+  						</tr>
+
+                    </table>
+
+    			</td>
+
+  			</tr>
+
+  			<tr>
+
+    			<td valign="top">
+
+    				<table width="750" border="0">
+
+  						<tr>
+
+						    <td width="260" valign="top">
+
+                            	<table width="100%" border="0">
+
+                                  <tr>
+
+                                    <td valign="middle" class="td3bluefont"><strong>G&Ouml;R&Uuml;ŞLER</strong></td>
+
+                                  </tr>
+
+                                  <tr>
+
+                                    <td class="td">
+
+<?php
+
+
+
+$result = mysql_query("SELECT * FROM idm_posts INNER JOIN idm_term_relationships ON id=object_id WHERE term_taxonomy_id=7 and post_status='publish'  order by post_date desc");
+
+$count_gorus=0;
+
+while($row = mysql_fetch_array($result))
+
+  {
+
+	  $result44 = mysql_query("SELECT * FROM idm_postmeta where meta_key='resim' and post_id=" . $row['ID']);
+
+		$row44 = mysql_fetch_array($result44);?>
+
+		<img src="<?php echo ($row44['meta_value']); ?>" width="100" height="80" align="left">
+
+        <?php
+
+  echo ("<strong> <a href='bilgibankasi_det.php?mn=3&p=" . $row['ID'] . "'>" . $row['post_title'] . "</a></strong><br>");
+
+  echo ($row['post_excerpt']);
+
+  echo ("<br><br> <img src='images/seperator.gif' width='200' height='1'><br><br>");
+
+  }
+
+?>
+
+                                    </td>
+
+                                  </tr>
+
+                                  <tr><td class="td"></td></tr>
+
+                                </table>
+
+
+
+                            </td>
+
+						    <td valign="top"><img src="images/seperator.gif" width="1" height="620">&nbsp;</td>
+
+						    <td valign="top">
+
+                           		<table width="450" border="0">
+
+                                  <tr>
+
+                                    <td valign="middle" class="td3bluefont"><strong>G&Uuml;NDEMDEKİLER</strong></td>
+
+                                  </tr>
+
+                                  <tr>
+
+                                    <td class="td">
+<?php
+
+
+
+	$result = mysql_query("SELECT * FROM idm_posts INNER JOIN idm_term_relationships ON id=object_id WHERE term_taxonomy_id=11 and post_status='publish' order by post_date DESC");
+
+
+
+while($row = mysql_fetch_array($result))
+
+  {
+
+	  $result4 = mysql_query("SELECT * FROM idm_postmeta where meta_key='resim' and post_id=" . $row['ID']);
+
+		$row4 = mysql_fetch_array($result4);?>
+
+		<p><img src="<?php echo ($row4['meta_value']); ?>" width="110" height="80" align="left">
+
+<?php
+
+  echo "<strong><a href='gundem_det.php?p=" . $row['ID'] . "'>" . $row['post_title'] . "</a></strong><br>";
+
+  echo $row['post_excerpt'];
+
+  echo "<br> <img src='images/seperator.gif' width='510' height='1'>";
+  ?>
+  </p>
+  <p></p>
+<?php 
+  }
+
+?>
+
+                                    </td>
+
+                                  </tr>
+
+                              </table>
+
+                        </td>
+
+						    <td>&nbsp;</td>
+
+						</tr>
+
+					</table>
+
+   				</td>
+
+              </tr>
+
+		</table>
+
+        </td>
+
+        <td valign="top">
+
+        			<table width="100%" height="100%" border="0" cellpadding="5" cellspacing="0">
+                      <tr>
+                        <td align="center" valign="top" class="td4blue"><strong>İÇ KONTROL SEVİYE BELİRLEME ANKETİ</strong></td>
+                      </tr>
+                      <tr>
+                        <td height="80" align="center" valign="top" class="td5">Firmanızın İ&ccedil; Kontrol Sisteminin Etkinliğini &Ouml;l&ccedil;mek İstiyorsanız<br>
+                        <br>
+                        <a href="ickontrol_anket.php?p=44" onMouseOver="MM_swapImage('Image182','','images/buttons/ankete_katilin_mo.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/buttons/ankete_katilin.gif" name="Image182" width="90" border="0"></a></td>
+                      </tr>
+                     <tr>
+                        <td height="5" valign="top"></td>
+                      </tr>
+                      <tr>
+                        <td align="center" valign="top" class="td4blue"><strong>İ&Ccedil; DENETİM ANKETİ</strong></td>
+                      </tr>
+                      <tr>
+                        <td height="80" align="center" valign="top" class="td5">İ&ccedil; Denetim Hakkında Bildiklerinizi Test Etmek İstiyorsanız<br>
+                        <br>
+                        <a href="icdenetim_anket.php?p=43" onMouseOver="MM_swapImage('Image181','','images/buttons/ankete_katilin_mo.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/buttons/ankete_katilin.gif" name="Image181" width="90" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td height="5" valign="top"></td>
+                      </tr>
+                      <tr>
+                        <td align="center" valign="top" class="td4blue"><strong>Danışma Hattı&nbsp;</strong></td>
+                      </tr>
+                      <tr>
+                        <td height="80" align="center" valign="top" class="td5">T&uuml;m Sorularınız İ&ccedil;in<br>
+                        <br>
+                        <a href="iletisim.php?p=1" onMouseOver="MM_swapImage('Image18','','images/buttons/basvuru_but_mo.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/buttons/basvuru_but.gif" name="Image18" width="90" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td height="5" valign="top"></td>
+                      </tr>
+                      <tr>
+                        <td align="center" valign="top" class="td4blue"><strong>D&uuml;zenli Haber</strong></td>
+                      </tr>
+                      <tr>
+                        <td height="100" align="center" valign="top" class="td5">Haber ve B&uuml;ltenlerimizi D&uuml;zenli Almak İstiyorsanız<br><br><iframe allowtransparency="yes" frameborder="0" scrolling="no" width="220" height="90" src="haberkayit_form.php"></iframe></td>
+
+                      </tr>
+                      <tr>
+                        <td height="5" valign="top"></td>
+                      </tr>
+                    </table>
+
+        </td>
+
+        </tr>
+
+      </table>
+
+			<!--<img src="images/index_04.gif" width="775" height="513" alt="">--></td>
+
+	</tr>
+
+	<tr>
+
+		<td colspan="2">
+
+<table width="100%" bgcolor="#E8E9EE" border="0" cellpadding="4" class="td">
+		  <tr>
+		    <td>Copyright &#304;DM</td>
+            <td></td>
+		    <td align="right" class="menutext"><a href="site_haritasi.php">Site Haritas&#305;</a> | <a href="genel.php?p=1413">Gizlilik Politikas&#305;</a> | <a href="genel.php?p=2043">Yasal Kurallar</a> | <a href="iletisim.php">&#304;leti&#351;im</a></td>
+	      </tr>
+		  </table>
+          <?php
+mysql_close($con);
+?> 
+			<!--<img src="images/index_05.gif" width="775" height="62" alt="">--></td>
+	</tr>
+</table>
+</td></tr></table>
+ <div id="div1" onMouseOut="this.style.visibility='hidden';" style="position:relative; left:700px; bottom:150px; visibility:hidden; width:505px;"><table width="500" bgcolor="#FFFFFF" border="1"><tr><td align="center" class="td">4904 SAYILI TÜRKİYE İŞ KURUMU KANUNU GEREĞİNCE,<br><br>
+İŞ ARAYANLARDAN ÜCRET ALINMASI YASAKTIR!<br><br>
+Şikayetleriniz için Türkiye İş Kurumu İl Müdürlüğüne başvurabilirsiniz.<br><br>
+<a href="http://www.iskur.gov.tr/LoadExternalPage.aspx?uicode=statbizeulasin" target="_blank">İletişim Bilgileri için t&#305;klay&#305;n&#305;z.</a></td></tr></table></div>
+<!-- End Save for Web Slices -->
+</body>
+</html>
